f74078c7595d481e505ef037ad1abfbe22bf83f0
Jonas Dorfinger <jonas.dorfinger@gmx.at>
2020-03-31 14:59:18 +0200

    implement getRandomEmoji() function & add fitzi feature

--- index.js
+++ index.js
@@ -20,6 +20,12 @@ client.on('message', (msg) => {
         });
     }
 
+    if (msg.author.id === '219585398618193920') {
+        msg.react(getRandomEmoji()).then(() => {
+            console.log(`reacted to message from ${msg.author.username} at ${new Date().toLocaleDateString()}`);
+        });
+    }
+
     if (msg.author.id === '305083169831649280') {
         msg.react('689473770297098333').then(() => {
             console.log(`reacted to message from ${msg.author.username} at ${new Date().toLocaleDateString()}`);
@@ -73,5 +79,11 @@ client.on('voiceStateUpdate', (oldMember, newMember) => {
         }
     }
 });
+
+function getRandomEmoji() {
+    const emojis = require('./emojis').emojis;
+
+    return emojis[Math.floor(Math.random() * emojis.length)];
+}
 
 client.login(token);
