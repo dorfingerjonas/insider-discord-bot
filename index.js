24682ed9b9ed60b962201781b4bbf97e2434e944
Jonas Dorfinger <jonas.dorfinger@gmx.at>
2020-03-31 10:57:33 +0200

    add philipp feature

--- index.js
+++ index.js
@@ -42,4 +42,17 @@ client.on('message', (msg) => {
     }
 });
 
+
+client.on('voiceStateUpdate', (oldMember, newMember) => {
+    if (newMember.channel.id === '687627580295348235' && newMember.member.user.id === '221695439160737792') {
+        newMember.guild.channels.cache
+            .get('690485778337366060')
+            .send('https://tenor.com/view/applausi-collettivi-standing-obation-clap-applause-gif-14346895');
+
+        newMember.guild.channels.cache
+            .get('690485778337366060')
+            .send(`<@${221695439160737792}>`);
+    }
+});
+
 client.login(token);
